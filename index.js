7ec1e7d0349e8ae4086067257b256aa1cd02f774
raasulov <raxmatovumed33@gmail.com>
2024-08-06 22:11:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+}
+
+function toggleDetails(id) {
+    var details = document.getElementById(id);
+    details.style.display = (details.style.display === 'block') ? 'none' : 'block';
+}
